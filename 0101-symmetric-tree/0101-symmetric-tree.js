91779915bfe8d55ecc2a124570f3d023038eb55e
Jaecheon Jeong <hijde@naver.com>
2023-06-20 16:52:59 +0900

    Time: 105 ms (5.06%), Space: 46.6 MB (5.01%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+
+  const LArray = [];
+  const RArray = [];
+    
+  const LTree = (node, array) => {
+    if (node) {
+      if (node.val) {
+        array.push(node.val);
+      }
+
+      if (node.left) {
+        LTree(node.left, array);
+      } else {
+        array.push(1);
+      }
+
+      if (node.right) {
+        LTree(node.right, array);
+      } else {
+        array.push(2)
+      }
+    }
+  }
+
+  const RTree = (node, array) => {
+    if (node) {
+      if (node.val) {
+        array.push(node.val);
+      }
+
+      if (node.right) {
+        RTree(node.right, array);
+      } else {
+        array.push(1);
+      }
+
+      if (node.left) {
+        RTree(node.left, array);
+      } else {
+        array.push(2);
+      }
+    }
+  }
+
+  if (root.left) {
+    LTree(root.left, LArray);
+  }
+
+  if (root.right) {
+    RTree(root.right, RArray);
+  }
+
+  console.log(LArray, RArray);
+
+  if (LArray.length !== RArray.length) return false;
+
+  for (let i = 0; i < LArray.length; i += 1) {
+    if (LArray[i] !== RArray[i]) {
+      return false;
+    } 
+  }
+
+  return true;
+};
