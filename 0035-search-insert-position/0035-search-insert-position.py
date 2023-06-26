7f92107ffc52c4ffd9804151137a8b81d18ac07c
Jaecheon Jeong <hijde@naver.com>
2023-06-26 19:00:07 +0900

    Time: 67 ms (45.48%), Space: 17.2 MB (49.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,14 +2,9 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         s_pos = 0
         e_pos = len(nums)-1
-        length = 0
-
 
         while s_pos <= e_pos:
-
             half_pos = (s_pos + e_pos) // 2
-
-            print("f", half_pos, nums[half_pos])
 
             if nums[half_pos] == target:
                 return half_pos
