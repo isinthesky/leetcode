d7116f39400864ac014823d1894408b35ada2a5c
Jaecheon Jeong <hijde@naver.com>
2023-06-29 00:16:43 +0900

    Time: 71 ms (31.07%), Space: 17.2 MB (47.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,49 +5,23 @@ class Solution:
         r = len(s) - 1
 
         def isChar(c: str) -> bool:
-            print("ccc",c, l, r)
-            if 'a' <= c and 'z' >= c:
+            if ('a' <= c and 'z' >= c) or ('0' <= c and '9' >= c):
                 return True
             else:
                 return False
 
 
         while l < r:
-            print("us", l, r)
-
-            while l < r and us[l].isalnum() == False: 
+            while l < r and isChar(us[l]) == False: 
                 l += 1
 
-            while r > l and us[r].isalnum() == False: 
+            while r > l and isChar(us[r]) == False: 
                 r -= 1
             
-            print(l, r, us[l], us[r])
             if us[l] != us[r] or l > r:
                 return False
             else:    
                 l += 1
                 r -= 1
             
-        
         return True
-
-
-
-        #   while l < r:
-        #     print("us", l, r)
-        #     if not isChar(us[l]) and l < r:
-        #         print("aa", l, r)
-        #         l += 1
-        #         continue
-
-        #     if not isChar(us[r]) and l < r:
-        #         print("ss", l, r)
-        #         r -= 1
-        #         continue
-            
-        #     print(l, r, us[l], us[r])
-        #     if us[l] != us[r] or l > r:
-        #         return False
-        #     else:    
-        #         l += 1
-        #         r -= 1
