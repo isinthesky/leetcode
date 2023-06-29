35c38101b71f57fc5d98f7a0a70841a37da46a5b
Jaecheon Jeong <hijde@naver.com>
2023-06-29 15:56:16 +0900

    Time: 43 ms (86.37%), Space: 16 MB (99.77%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,17 +1,31 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        position = len(digits) - 1
-        dec = True
+    #     position = len(digits) - 1
+    #     dec = True
         
-        while position >= 0:
-            num = digits[position] + (1 if dec else 0)
-            digits[position] = num % 10
+    #     while position >= 0:
+    #         num = digits[position] + (1 if dec else 0)
+    #         digits[position] = num % 10
 
-            dec = True if num > 9 else False
+    #         dec = True if num > 9 else False
 
-            position -= 1
+    #         position -= 1
         
-        if dec :
-            digits.insert(0,1)
+    #     if dec :
+    #         digits.insert(0,1)
         
-        return digits
+    #     return digits
+
+        string = ""
+        for num in digits:
+            string += str(num)
+        
+        number = str(int(string)+1)
+
+        arr = []
+
+        for num in number:
+            arr.append(int(num))
+
+        return arr
+
