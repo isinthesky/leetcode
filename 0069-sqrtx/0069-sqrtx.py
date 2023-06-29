5c27bdf10edb3b9b86c3dca6c001d3410ccc90bd
Jaecheon Jeong <hijde@naver.com>
2023-06-29 21:22:37 +0900

    Time: 51 ms (71.9%), Space: 16.4 MB (23.2%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,16 +6,13 @@ class Solution:
         left,right=1,x
         
         while left<=right:
-            mid=(left+right)//2
-
-            print("mm",mid,left,right)
+            mid=(left+right) // 2
 
             if mid*mid==x:
                 return mid
             if mid*mid>x:
                 right=mid-1
             else:
-                print("el",mid*mid)
                 left=mid+1
         return right
 
