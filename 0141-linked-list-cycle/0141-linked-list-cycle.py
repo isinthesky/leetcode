b0d89b37524f78586db662b8ed0fd5ec976a739e
Jaecheon Jeong <hijde@naver.com>
2023-06-30 20:51:19 +0900

    Time: 504 ms (5.29%), Space: 20.9 MB (9.81%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,24 +7,41 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        if not head :
+        if not head:
             return False
 
-        slow = head
-        fast = head.next
+        node = head
+        mem = None
+
+        arr = []
+
+        pos = 0 
+        def compArr(cnode: Optional[ListNode]) -> bool:
+            temp = set()
+            while cnode:
+                if cnode in temp:
+                    return True
+                temp.add(cnode)
+                cnode = cnode.next
+
+            return False
+
+        while node :
+            if node.val in arr:
+                pos = arr.index(node.val)
+
+                if compArr(node) == True:
+                    return True
+                else:
+                    return False
+
+            else:
+                arr.append(node.val)
+                node = node.next
+
+    
+
         pos = -1
-
-        while slow != fast :
-
-            # print(slow.val, fast.val)
-            if not fast or not fast.next :
-                return False
-            slow = slow.next
-            pos += 1
-
-            fast = fast.next.next
-
-        print(pos)
-        return True
-            
-
+        return False
+        
+        
