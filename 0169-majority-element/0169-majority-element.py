c69bdb05a0c25017d17f5e4352d85ff770f22238
Jaecheon Jeong <hijde@naver.com>
2023-07-02 19:18:12 +0900

    Time: 164 ms (93.54%), Space: 17.9 MB (73.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,26 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dicAdd = {}
-        
-        for n in nums:
-            if n in dicAdd:
-                dicAdd[n] += 1
-            else :
-                dicAdd[n] = 1
-        
-        dicSort = {}
-        max = 0
-        
-        for key, num in dicAdd.items():
-            if max <= num :
-                max = num
-                if num in dicSort:
-                    val = dicSort.get(max)
-                    if val < key :
-                        dicSort[max] = key
-                else :
-                    dicSort[max] = key
-                
-                
+        half = len(nums) // 2
+        nums.sort()
 
-        return dicSort.get(max)
+
+        return nums[half]
+        
