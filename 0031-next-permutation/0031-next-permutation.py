e380f71c99995bc72976df2bda9e97b43b46e765
Jaecheon Jeong <hijde@naver.com>
2023-07-07 16:50:45 +0900

    Time: 56 ms (66.03%), Space: 16.3 MB (40.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,12 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        i = j = length = len(nums) - 1
+        i = length = len(nums) - 1
 
         while i > 0 and nums[i-1] >= nums[i]:
             i -= 1
-
-        print(i) # pos = 2 num = 3
 
         if i == 0: 
             nums.reverse()
@@ -22,9 +20,6 @@ class Solution:
 
         nums[i-1], nums[k] = nums[k], nums [i-1]
 
-
-        # nums[i - 1], nums[length] = nums[length], nums[i - 1]
-
         l = i
         r = length
 
@@ -32,5 +27,3 @@ class Solution:
             nums[l], nums[r] = nums[r], nums[l]
             l += 1
             r -= 1
-
-        print(nums)
