fc5354a56a6f77460df4fb5b45b58252999543ef
Jaecheon Jeong <hijde@naver.com>
2023-07-11 14:28:27 +0900

    Time: 66 ms (58.53%), Space: 16.5 MB (42.24%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.py
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.py
@@ -5,35 +5,29 @@ class Solution:
         # 음수 없애기
         # 짝수 2번 적용 무효화 
         # 홀수 1번 적용 최소 값 찾기
-        minus = []
+        minus = 0
         nums.sort()
 
         for n, v in enumerate(nums):
             if v < 0:
-                minus.append([n,v])
-
-        print(minus, nums)
+                minus += 1
 
         pos = 0
-        minus_count = len(minus)
         while k > 0:
-            if minus_count < 1:
+            if minus < 1:
                 if k % 2 == 1 :
                     nums.sort()
                     nums[0] = -(nums[0])
-                    k -= 1
                 else:
                     break
             else :
-                if minus_count < 1 and (k % 2 == 0):
+                if minus < 1 and (k % 2 == 0):
                     break
                 else:
                     nums[pos] = -(nums[pos])
-                    k -= 1
                     pos += 1
-                    minus_count -= 1
-                    print(nums, k, pos, minus_count)
-        
-        print(nums)
+                    minus -= 1
             
+            k -= 1
+        
         return sum(nums)
