a8ac5c16d0921c7aa39b782f64b27f2bdb3fd2f2
Jaecheon Jeong <hijde@naver.com>
2023-07-31 18:57:35 +0900

    Time: 91 ms (68.15%), Space: 50.4 MB (33.55%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,12 +10,8 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    
-    if (!head)
-        return;
-    
-    if (!head.next)
-        return head;
+    if (!head) return;
+    if (!head.next) return head;
     
     const stack = []
     let node = head
@@ -28,32 +24,20 @@ var reorderList = function(head) {
     }
 
     node = head
-        
-    console.log(length, stack, Math.floor(stack.length/2))
-
-    
     
     for (let i = 0; i < Math.floor(length/2); i += 1)
     {
-        console.log(i)
         getNode = stack.pop();
-        console.log(11)
         nextNode = node.next;
-        console.log(22)
 
         node.next = getNode;
-        console.log(33)
         getNode.next = nextNode;
-        console.log(44)
         node = nextNode;
-        console.log(55)
     }
     
-    // if node.next
     if (length % 2 > 0) {
         node.next = null
     } else {
         node.next.next = null
     }
-
 };
