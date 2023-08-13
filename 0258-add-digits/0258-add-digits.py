cb843c8fc5dc23fcca576cc1f96a3db1a061b563
Jaecheon Jeong <hijde@naver.com>
2023-08-14 08:59:37 +0900

    Time: 44 ms (66.67%), Space: 16.2 MB (89.64%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        def digitFunc(number: int)-> int:
+            digit = 0
+            for n in str(number):
+                digit += int(n)
+            return digit
+
+        while num >= 10:
+            num = digitFunc(num)
+
+        return num
+        
