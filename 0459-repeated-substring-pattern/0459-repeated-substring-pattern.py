295f9be4838d39dd976f4469b6db50a7f8b3981b
Jaecheon Jeong <hijde@naver.com>
2023-09-07 09:44:52 +0900

    Time: 94 ms (29.56%), Space: 16.8 MB (7.07%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -4,24 +4,20 @@ class Solution:
         s_length = len(s)
         
         for ch in s[1:]:
-
-            if repeat[0] == ch:
-                repeat_length = len(repeat)
-
+            repeat_length = len(repeat)
+            
+            if repeat[0] == ch and (s_length % repeat_length) == 0:
                 if repeat_length > s_length / 2:
                     return False
 
-                if (len(s) % repeat_length) != 0:
-                    repeat += ch
+                merge = set()
+                for i in range(0, s_length, repeat_length):
+                    merge.add(s[i:i+repeat_length])
+                
+                if len(merge) == 1:
+                    return True
                 else:
-                    merge = set()
-                    for i in range(0, len(s), repeat_length):
-                        merge.add(s[i:i+repeat_length])
-                    
-                    if len(merge) == 1:
-                        return True
-                    else:
-                        repeat += ch
+                    repeat += ch
             else:
                 repeat += ch
 
