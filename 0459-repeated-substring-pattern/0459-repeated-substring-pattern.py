ef671db1ce71f788f76192c8992f97f0487e5c04
Jaecheon Jeong <hijde@naver.com>
2023-09-07 09:31:40 +0900

    Time: 77 ms (33.49%), Space: 16.7 MB (7.07%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,14 +1,20 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         repeat = s[0]
+        s_length = len(s)
         
         for ch in s[1:]:
+
             if repeat[0] == ch:
-                if (len(s) % len(repeat)) != 0:
+                repeat_length = len(repeat)
+
+                if repeat_length > s_length / 2:
+                    return False
+
+                if (len(s) % repeat_length) != 0:
                     repeat += ch
                 else:
                     merge = set()
-                    repeat_length = len(repeat)
                     for i in range(0, len(s), repeat_length):
                         merge.add(s[i:i+repeat_length])
                     
@@ -20,4 +26,3 @@ class Solution:
                 repeat += ch
 
         return False
-        
