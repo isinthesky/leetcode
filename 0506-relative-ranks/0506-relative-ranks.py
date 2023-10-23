b0cab927dd78c4872577cba429cd2b3a1fbe2b5b
Jaecheon Jeong <hijde@naver.com>
2023-10-23 10:05:54 +0900

    Time: 65 ms (89.99%), Space: 17.8 MB (13.47%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,16 +1,13 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        rank = ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+        rank = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        result = [ "" for i in range(len(score)) ]
         ranker = {}
         
         for pos, point in enumerate(score):
             ranker[point] = pos
         
         sortedRanker = sorted(ranker.items(), reverse=True)
-
-        print(sortedRanker)
-
-        result = [ "" for i in range(len(score)) ]
 
         for pos, value in enumerate(sortedRanker):
             result[value[1]] = str(pos + 1)
@@ -20,11 +17,5 @@ class Solution:
                 break
             result[sortedRanker[pos][1]] = rank[pos]
 
-        print(result)
-        
-           
-            
         return result
-
-        
         
